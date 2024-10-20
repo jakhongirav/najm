5413ec4697f68fc793a4d396b1a94365180e8f56
“jakhongirav” <jakhongir.av@gmail.com>
2024-10-20 23:37:17 +0500

    initial commit

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -5,15 +5,8 @@ import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 const Payment = () => {
   return (
     <div className="container mx-auto">
-      <Breadcrumbs title="Payment gateway" />
-      <div className="pb-10">
-        <p>Payment gateway only applicable for Production build.</p>
-        <Link to="/">
-          <button className="w-52 h-10 bg-primeColor text-white text-lg mt-4 hover:bg-black duration-300">
-            Explore More
-          </button>
-        </Link>
-      </div>
+      <Breadcrumbs title="Оформление заказа" />
+      <div className="pb-10"></div>
     </div>
   );
 };
