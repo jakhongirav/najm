2996e9619bbf368a33617dd2b8a70c07212ed2ae
jakhongirav <jakhongir.av@gmail.com>
2024-11-12 21:51:50 +0100

    favico && metadata

--- src/components/home/NewArrivals/NewArrivals.js
+++ src/components/home/NewArrivals/NewArrivals.js
@@ -8,40 +8,43 @@ import { useDispatch, useSelector } from "react-redux";
 import { getNewArrivals } from "../../../redux/orebiSlice";
 
 const NewArrivals = () => {
-  const settings = useMemo(() => ({
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
+  const settings = useMemo(
+    () => ({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          },
         },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+          },
         },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+          },
         },
-      },
-    ],
-  }), []);
+      ],
+    }),
+    []
+  );
 
   const dispatch = useDispatch();
 
